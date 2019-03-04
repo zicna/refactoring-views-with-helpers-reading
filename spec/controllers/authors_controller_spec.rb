3afb8e58245f8ddf5efe7c144bea0c8166df25ea
maxwellbenton <maxwellbenton@gmail.com>
2019-03-04 14:43:30 -0500

    updates deprecated controller teststyle

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe AuthorsController, type: :controller do
   describe "GET #show" do
     it "returns http success" do
       Author.create(name: "Dennis", genre: "Ye Old Music", bio: "A really good artist")
-      get :show, {:id => 1}
+      get :show, params: {:id => 1}
       expect(response).to have_http_status(:success)
     end
   end
